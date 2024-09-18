86175b0bb54e2c907b067b391edb5a4e8ad20b0f
KS-Anandh <167880483+KS-Anandh@users.noreply.github.com>
2024-09-18 07:00:43 +0530

    Update NavBar.jsx
    WebName Changed

--- client/src/componets/NavBar.jsx
+++ client/src/componets/NavBar.jsx
@@ -17,7 +17,7 @@ const NavBar = () => {
     <div className='navBar' >
        <div style={{display:"flex",alignItems:"center",justifyContent:"space-around",columnGap:"30px",height:"120px"}}>
        <img src={logo} alt="" width={80} height={80} />
-       <h3 style={{color:"rgb(204, 3, 93)",fontSize:"25px"}}><span style={{color:"rgb(44, 6, 50)"}}>IOT</span>EX</h3>
+       <h3 style={{color:"rgb(204, 3, 93)",fontSize:"25px"}}><span style={{color:"rgb(44, 6, 50)"}}>IOT</span>EXT</h3>
        </div>
        <input id="checkBox" onChange={e=> setNavBar(e.target.checked)} defaultChecked={true} type='checkBox' style={{display:"none"}}/>
      {navBar && !navState==1?<label htmlFor='checkBox'><img src={more}  width={30} height={30}/></label> :navBar==1?"":< label htmlFor='checkBox'><img src={cross} width={30} height={30}/></label>}  
