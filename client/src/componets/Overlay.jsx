90da1a004651729881a32d4e2cf3fcdefbe8f893
KS-Anandh <167880483+KS-Anandh@users.noreply.github.com>
2024-10-20 21:02:50 +0530

    Create Overlay.jsx

---
+++ client/src/componets/Overlay.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Overlay = () => {
+  return (
+    <>
+    <div className="overlay">
+      <div className='dots'>
+      <div className='dot1 dot'></div>
+      <div className='dot2 dot'></div>
+      <div className='dot3 dot'></div>
+      <div className='dot4 dot'></div>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default Overlay
